89326830348787294d1a8d9f8aaf40d25c199164
Elliot Friedman <efriedma@e1z2r6p6.42.us.org>
2018-07-20 10:34:14 -0700

    Non plant class

---
+++ pvp/non_plant.py
@@ -0,0 +1,13 @@
+from organism import Organism
+
+class Non_Plant(Organism):
+    worth = 20
+    
+    def __init__(self):
+        self.hp = 80
+        self.dmg = 5
+
+    def attack(self, plant):
+        plant.hp -= self.dmg
+
+
