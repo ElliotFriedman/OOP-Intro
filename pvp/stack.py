ea9937a29f9f6d2e30afd3a1be4b550f98031c50
Elliot Friedman <efriedma@e1z2r6p6.42.us.org>
2018-07-20 10:48:53 -0700

    Added Stack and Queue Classes

---
+++ pvp/stack.py
@@ -0,0 +1,50 @@
+class Node():
+    def __init__(self, value, next = None):
+        self.data = value
+        self.next = next
+
+class Stack(object):
+    
+    def __init__(self, node = None):
+        self._size = 0
+        self.head = node
+
+    def isEmpty(self):
+        if self.head is None:
+            return None
+        else:
+            return False
+
+    def push(self, data):
+        n = Node(data)
+        n.next = self.head
+        self._size += 1
+        self.head = n
+
+    def pop(self):
+        if self.head is not None:
+            if self.head.next:
+                self.head = self.head.next
+            else:
+                self.head = None
+            self._size -= 1
+
+    def peek(self):
+        if self.head is not None:
+            return self.head.data
+        return None
+
+    @property
+    def size(self):
+        return self._size
+
+    def __str__(self):
+        tmp = self.head
+        _str = "["
+        while tmp is not None:
+            if tmp.next is not None:
+                _str += (str(tmp.data) + ", ")
+            else:
+                _str += str(tmp.data)
+            tmp = tmp.next
+        return (_str + "]")
